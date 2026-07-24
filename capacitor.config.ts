9bf453015964aec82295b1a8cbfdc34467d68005
Maraphuc <135527445+Maraphuc@users.noreply.github.com>
2026-07-24 11:58:45 +0700

    build: simplify Capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.maraphuc.lichsudang',
   appName: 'Lịch Sử Đảng',
   webDir: 'dist',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
   }
